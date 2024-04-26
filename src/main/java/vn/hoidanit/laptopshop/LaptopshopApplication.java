9a5a00932eebe26881f3880132e293d6d2d10a66
tn9th8 <tn9th8@gmail.com>
2024-04-26 23:15:20 +0700

    spring beans

--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -2,12 +2,22 @@ package vn.hoidanit.laptopshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
+		// app = container
 		SpringApplication.run(LaptopshopApplication.class, args);
+
+		// ApplicationContext context =
+		// SpringApplication.run(LaptopshopApplication.class, args);
+
+		// // print list of beans:
+		// for (String s : context.getBeanDefinitionNames()) {
+		// System.out.println(s);
+		// }
 	}
 
 }
